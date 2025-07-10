f54eedcb504daba5d600e2728f98388d93fb8d12
maoo abdalla <maoo@bintzone.com>
2025-07-10 13:17:10 +0300

    aded

--- src/hooks/fetch/useFetch.tsx
+++ src/hooks/fetch/useFetch.tsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 
-export const API_BASE_URL = '';
+export const API_BASE_URL = 'dglserver-production.up.railway.app';
 
 type FetchOptions = {
   url: string;
--- vite.config.ts
+++ vite.config.ts
@@ -24,7 +24,7 @@ export default defineConfig({
     port: 3000,
     proxy: {
       '/api': {
-        target: 'http://localhost:5000',
+        target: 'http://dglserver-production.up.railway.app',
         changeOrigin: true,
         // rewrite: (path) => path.replace(/^\/api/, ''),
       },
